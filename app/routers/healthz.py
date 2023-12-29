4ec72ad1f372a4c66719ab3ef43e34ccd51de152
Esther Fernandez <estherfb_3397@hotmail.com>
2023-12-29 13:39:26 +0100

    cd run DO DEV

--- app/routers/healthz.py
+++ app/routers/healthz.py
@@ -1,14 +1,17 @@
-from  fastapi import APIRouter
+from fastapi import APIRouter
 
-router = APIRouter(prefix="",tags=["healthz"])
+router = APIRouter(prefix="", tags=["healthz"])
+
 
 @router.get("/healthzReadiness", include_in_schema=False)
 async def get_healthz_readiness() -> str:
     return "OK"
 
+
 @router.get("/healthzLiveness", include_in_schema=False)
 async def get_healthz_liveness() -> str:
     return "OK"
+
 
 @router.get("/healthzStartup", include_in_schema=False)
 async def get_healthz_startup() -> str:
--- app/routers/hello_world.py
+++ app/routers/hello_world.py
@@ -6,3 +6,8 @@ router = APIRouter(prefix="/api/v1/hello-world", tags=["hello-world"])
 @router.get("")
 async def get_hello_world() -> str:
     return "Hello World!"
+
+
+@router.get("")
+async def sum(x: float, y: float) -> float:
+    return x + y
